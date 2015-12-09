a1e5a529b196ec83b0364a1dfaf137c04a777b2e
Garion Herman <cheddam@me.com>
2015-12-09 23:34:48 +1300

    Progress as of 9-12-2015

--- resources/assets/js/components/fill.js
+++ resources/assets/js/components/fill.js
@@ -1,6 +1,9 @@
 var Goal = React.createClass({
 	handleClick: function() {
-		this.props.handleComplete(this.props.id);
+		store.dispatch({
+			type: 'TOGGLE_COMPLETED',
+			id: this.props.id
+		}).bind(this);
 	},
 	render: function() {
 		return (
@@ -19,7 +22,7 @@ var Category = React.createClass({
 				<h4>{this.props.name}</h4>
 				{this.props.goals.map(function(goal) {
 					return (
-						<Goal key={goal.id} id={goal.id} name={goal.name} points={goal.points} completed={goal.completed} handleComplete={this.props.handleComplete} />
+						<Goal key={goal.id} id={goal.id} name={goal.name} points={goal.points} completed={goal.completed} />
 					);
 				}, this)}
 			</div>
@@ -33,7 +36,7 @@ var CategoryList = React.createClass({
 			<div className="category-list">
 				{this.props.data.map(function(category) {
 					return (
-						<Category key={category.name} name={category.name} goals={category.goals} handleComplete={this.props.handleComplete} />
+						<Category key={category.name} name={category.name} goals={category.goals} />
 					);
 				}, this)}
 			</div>
@@ -42,62 +45,98 @@ var CategoryList = React.createClass({
 });
 
 var GoalInterface = React.createClass({
-	loadGoalsFromServer: function() {
-		$.ajax({
-			url: '/goals/list',
-			dataType: 'json',
-			cache: false,
-			success: function(data) {
-				this.setState({data: data});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	},
-	handleComplete: function(id) {
-		$.ajax({
-			url: '/goals/complete/' + id,
-			dataType: 'json',
-			type: 'POST',
-			data: {
-				'_token': $('meta[name=csrf-token]').attr('content')
-			},
-			success: function(data) {
-				var localCats = this.state.data;
-
-				for (var c = 0; c < localCats.count; c++) {
-					for (var g = 0; g < localCats[c].goals.count; g++) {
-						if (localCats[c].goals[g].id === data.goal_id) {
-							localCats[c].goals[g].completed = true;
-						}
-					}
-				}
-
-				this.setState({data: localCats});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
-		});
-	},
-	getInitialState: function() {
-		return {data: []};
-	},
-	componentDidMount: function() {
-		this.loadGoalsFromServer();
-		setInterval(this.loadGoalsFromServer, this.props.pollInterval);
-	},
 	render: function() {
 		return (
 			<div className="pure-u-1 goal-interface">
-				<CategoryList data={this.state.data} handleComplete={this.handleComplete} />
+				<CategoryList goals={this.props.goals} />
 			</div>
 		);
 	}
 });
 
-ReactDOM.render(
-	<GoalInterface pollInterval={2000} />,
-	document.getElementById('goals')
-)
+// MODEL FROM HERE ON OUT BITCHES
+
+const goals = function(state = [], action) {
+
+	switch (action.type) {
+		case 'TOGGLE_COMPLETED':
+			return state;
+		case 'ADD_GOALS':
+			return state.concat(action.goals);
+	}
+
+}
+
+const todoApp = Redux.combineReducers({ goals });
+
+const store = Redux.createStore(todoApp);
+
+// Load ze goalzzzz
+$.ajax({
+	url: '/goals/list',
+	dataType: 'json',
+	cache: false,
+	success: function(data) {
+		store.dispatch({
+			type: 'ADD_GOALS',
+			goals: data
+		});
+	}.bind(this),
+	error: function(xhr, status, err) {
+		console.error('goal retrieval: ', status, err.toString());
+	}.bind(this)
+});
+
+const render = function() {
+
+	ReactDOM.render(
+		<GoalInterface goals={store.getState().goals} />,
+		document.getElementById('goals')
+	);
+
+}
+
+store.subscribe(render);
+render();
+
+/*
+
+Actions
+
+TOGGLE_COMPLETED
+
+*/
+
+
+// $.ajax({
+// 	url: '/goals/complete/' + id,
+// 	dataType: 'json',
+// 	type: 'POST',
+// 	data: {
+// 		'_token': $('meta[name=csrf-token]').attr('content')
+// 	},
+// 	success: function(data) {
+// 		var localCats = this.state.data;
+
+// 		for (var c = 0; c < localCats.count; c++) {
+// 			for (var g = 0; g < localCats[c].goals.count; g++) {
+// 				if (localCats[c].goals[g].id === data.goal_id) {
+// 					localCats[c].goals[g].completed = true;
+// 				}
+// 			}
+// 		}
+
+// 		this.setState({data: localCats});
+// 	}.bind(this),
+// 	error: function(xhr, status, err) {
+// 		console.error(this.props.url, status, err.toString());
+// 	}.bind(this)
+// });
+
+	// loadGoalsFromServer: function() {
+
+	// },
+	// componentDidMount: function() {
+	// 	this.loadGoalsFromServer();
+	// 	setInterval(this.loadGoalsFromServer, this.props.pollInterval);
+	// },
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -16,6 +16,7 @@
 
 	@yield('content')
 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/redux/3.0.4/redux.js"></script>
 	<script src="https://fb.me/react-0.14.2.js"></script>
 	<script src="https://fb.me/react-dom-0.14.2.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
